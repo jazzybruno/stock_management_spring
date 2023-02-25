318037384a3d70895102a7d5820a2b17f8c5a10b
jazzybruno <95282931+jazzybruno@users.noreply.github.com>
2023-02-25 09:15:41 +0200

    Added the orders model and annotations

--- src/main/java/rw/jazzybruno/istock/v1/dto/Orders.java
+++ src/main/java/rw/jazzybruno/istock/v1/dto/Orders.java
@@ -1,5 +1,43 @@
 package rw.jazzybruno.istock.v1.dto;
 
-public class Orders {
+import com.sun.istack.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "orders")
+public class Orders {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long order_id;
+    @NotNull
+    private String order_description;
+    @NotNull
+    private int quantity;
+    @NotNull
+    private LocalDate orderDate;
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "prod_id")
+    private Product product;
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public Orders(String order_description, int quantity, LocalDate orderDate, Product product, User user) {
+        this.order_description = order_description;
+        this.quantity = quantity;
+        this.orderDate = orderDate;
+        this.product = product;
+        this.user = user;
+    }
 }
